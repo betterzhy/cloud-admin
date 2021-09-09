3b0b4a6a442b740c5c5ecc71e532f8e36debd23a
betterzhy <1578613811@qq.com>
2021-09-10 00:12:04 +0800

    项目构建初始化

---
+++ cloud-admins/src/main/java/com/zhy/CloudAdminApplication.java
@@ -0,0 +1,11 @@
+package com.zhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAdminApplication.class, args);
+    }
+}
---
+++ cloud-category/src/main/java/com/zhy/CloudCategoryApplication.java
@@ -0,0 +1,11 @@
+package com.zhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCategoryApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CloudCategoryApplication.class, args);
+    }
+}
